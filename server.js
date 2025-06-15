3d6a19e1393c87c7186cac57140c8b2f7ff63541
Kanak <kanakdhiman3669@gmail.com>
2025-06-15 20:18:45 +0530

    Initial commit: Set up MERN stack application with routes for products, users, bids, and favorites. Added CORS support and AWS S3 integration for image uploads. Configured MongoDB connection and server settings.

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+require("dotenv").config();
+var passport = require("passport");
+var authenticate = require("./authenticate");
+
+// Loading routers
+const productRouter = require("./routes/api/productRouter");
+const userRouter = require("./routes/api/userRouter");
+const bidRouter = require("./routes/api/bidRouter");
+var favoriteRouter = require("./routes/api/favoriteRouter");
+
+const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  next();
+});
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB config
+const mongoURI = process.env.mongoURI;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+app.use(passport.initialize());
+
+// Use routes
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/bids", bidRouter);
+app.use("/api/favorites", favoriteRouter);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started running on port ${port}`));
